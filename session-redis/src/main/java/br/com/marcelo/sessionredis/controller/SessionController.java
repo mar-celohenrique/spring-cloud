4a2e04d0f35661d6319d766979cc54bfa9324585
Marcelo Henrique Pereira <marcelo_pereira@virgo.atlantico.net.br>
2018-02-08 11:54:51 -0200

    Adição do projeto para integrar o Spring Session com o Redis

---
+++ session-redis/src/main/java/br/com/marcelo/sessionredis/controller/SessionController.java
@@ -0,0 +1,14 @@
+package br.com.marcelo.sessionredis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionController {
+
+    @RequestMapping("/")
+    public String helloAdmin(){
+        return "Hello Admin!";
+    }
+
+}
